02bdc8167554b018dfe983fc6e6ccc337883eec5
Gilberto Valenzuela <ernestogilberto@hotmail.com>
2021-07-12 19:35:22 -0500

    Cambios responsive y nuevo menu

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,4 +1,6 @@
+const navegation = document.getElementById('nav')
 const button = document.getElementById('hamburger')
 button.addEventListener('click', () => {
     button.classList.toggle('active')
+    navegation.classList.toggle('active')
 })
