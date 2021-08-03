12d3a023e4f20dd27a104c8608fdc27e3e15b1be
Gilberto Valenzuela <ernestogilberto@hotmail.com>
2021-08-03 15:07:46 -0500

    Solo cambiando a Ubuntu y corrigiendo cosas menores.

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,6 +1,6 @@
-const navegation = document.getElementById('nav')
+const navigation = document.getElementById('nav')
 const button = document.getElementById('hamburger')
 button.addEventListener('click', () => {
     button.classList.toggle('active')
-    navegation.classList.toggle('active')
+    navigation.classList.toggle('active')
 })
